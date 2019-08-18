94bd3994debd5275eaf313a2f7e1109811fcd86b
Jason Kao <jason.kao@columbia.edu>
2019-08-18 18:07:40 -0400

    im out

--- script.R
+++ script.R
@@ -1,8 +1,22 @@
 library(jsonlite)
 library(tidyverse)
+library(tidytext)
+library(widyr)
 
 speeches <- fromJSON("speeches.json")
-speeches %>%
-  ggplot(aes(reorder(speaker, readability), readability)) +
-  geom_col() +
-  coord_flip()
+bigrams <- speeches %>% 
+  unnest_tokens(bigram, text, token = "ngrams", n = 2) %>% 
+  separate(bigram, c("word1", "word2"), sep = " ") %>% 
+  filter(!word1 %in% stop_words$word) %>% 
+  filter(!word2 %in% stop_words$word)
+
+words <- speeches %>% 
+  unnest_tokens(word, text) %>% 
+  filter(!word %in% stop_words$word)
+
+words %>% 
+  pairwise_count(word, speaker, sort = TRUE)
+
+bigrams %>%
+  # group_by(speaker) %>% 
+  count(word1, word2, sort = TRUE)
